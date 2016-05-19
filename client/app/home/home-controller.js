a733224429372de81e1e90ab80730b413a979efe
mishasaggi <monisha.saggi@gmail.com>
2016-05-19 12:13:50 -0500

    Add images, add user info in views and update css.

--- client/app/home/home-controller.js
+++ client/app/home/home-controller.js
@@ -4,15 +4,34 @@ angular.module('app.home', [])
   .controller('HomeController', ["$scope", "$location", "UserSearch", function($scope, $location, UserSearch) {
     $scope.userInput = {} //searchtags
     $scope.userInputError = false;
-    $scope.items = [];
+    $scope.posts = {
+      items : [ 'placeholder']
+    };
+
+    // //user data structure, put into mongo after admin / edit piece is
+    $scope.posts.users = {
+        longhorn: 'ruminating over javascript',
+        llama: 'making art from css',
+        'dumbo octopus': 'pranking people with computer programs',
+        'snowy owl': "creating magic (oh wait, it's just code)",
+        squirrel: 'collecting weird (awesome!) programs',
+        orangutan: 'learning web programming'
+    };
+
+    $scope.posts.images = {
+        longhorn: '../../images/longhorn.jpg',
+        llama: '../../images/llama.jpg',
+        'dumbo octopus': '../../images/dumbo octopus.jpg',
+        'snowy owl': '../../images/snowy owl.jpg',
+        squirrel: '../../images/squirrel.jpg',
+        orangutan: '../../images/orangutan.jpg'
+    };
 
     $scope.getAllPosts = function(){
       UserSearch.getPosts()
       .then(function(results){
-        console.log('results from getAll: ', results.results.data[0].answers[0]);
-        $scope.items = results.results.data;
-        $scope.items[0].answers[0].text = "testing answer. It will be a paragraph and needs formatting";
-        $scope.items[0].answers[0].user = "monkey";
+        console.log('results from getAll: ', results.results);
+        $scope.posts.items = results.results.data;
       })
     }
 
