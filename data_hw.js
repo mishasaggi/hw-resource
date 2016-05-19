91146abeee6cdd18108083174c584f1e593b6ffb
mishasaggi <monisha.saggi@gmail.com>
2016-05-19 13:10:55 -0500

    .gitignore

---
+++ data_hw.js
@@ -0,0 +1,107 @@
+
+  { "user": "orangutan",
+    "question": "Ummm how do I use this thing?",
+    "answers": [
+      { "user": "snowy owl", "text": "search for key words like <p>, paragraph, html and tags!"},
+      { "user": "longhorn", "text": "As a web developer we always have to find out how to do something or how something works, it's not always easy to pop open a book to find a very specific behavior/ thing we are looking for. So, we search developer sites like stack overflow and search on google to see what other web developers are doing. This is a site like that. Look around and see if anything helps you or gives you more ideas. Have fun!"},
+      { "user": "llama", "text": "Ask me questions, I'll try my best to answer them, that is find out if a similar question has already been asked and what was the discussion all about. Web developers frequently help each other by sharing their knowledge."},
+      { "user": "dumbo octopus", "text": "This is a mysterious place! Let's see if you figure it aaaallll out."}, 
+      { "user": "squirrel", "text": "Some questions are listed on this page that are commonly asked by other students learning the topic. Feel feel to browse and see if anything helps!"}, 
+      ],
+    "tags": ["ideas"]},
+
+  { "user": "orangutan",
+    "question": "why do I need an html tag?",
+    "answers": [
+      { "user": "snowy owl", "text": "Remember, tags tell the browsers how to display information."}, 
+      { "user": "squirrel", "text": "Every HTML document begins and ends with the <html> tag. This tells the browser that the following document is an html file."}
+      ],
+    "tags": ["html", "tag", "tags"]},
+
+  { "user": "orangutan",
+    "question": "where do I put a div tag?",
+    "answers": [
+      { "user": "snowy owl", "text": "Div tags go in your body tag, you can have as many div tags and you want and can also have div tags within div tags!"},
+      { "user": "squirrel", "text": "Remember to close out your tags though!"}
+      ],
+    "tags": ["tags", "tag", "div", "divs", "<div>"]},
+
+  { "user": "orangutan",
+    "question": "how do I make a list of things?",
+    "answers": [
+      { "user": "snowy owl", "text": "Use the <li> tag! Put each item on your list inside a <li> tag. For example: <li> pack backpack for tomorrow. </li> ."},
+      { "user": "squirrel", "text": "Remember put all the <li> tags inside one <ul> tag! That will tell the browser that a list is about to follow. Also, remember to close the <ul> tag when done :) "}
+      ],
+    "tags": ["tags", "tag", "<li>", "list"]},
+
+  { "user": "orangutan",
+    "question": "Why do I need divs? Why can't I put things in the body tag directly?",
+    "answers": [
+      { "user": "squirrel", "text": "You can put the other tags directly in the body (it will still work!) but div tags help you make little containers or boxes within your web page where you can put one type of thing. Think of this like organizing your desk. For example: You can have a div for "things to do today!" and another one for "What I accomplished this week" and you can them differently by just chaging the div and not each item in the div."}
+      ],
+    "tags": ["div", "tags", "tag", "body", "<body>"]},
+
+  { "user": "orangutan",
+    "question": "How should I use a paragraph ( <p> ) tag?",
+    "answers": [
+      { "user": "snowy owl", "text": "<p> tags are for seperating out different sections of your text. Thing about an essay, if you wanted a part to be highlighted and another to be bold and standing out, those sections would go in a <p> tag of their own so you can make them look different."}
+      ],
+    "tags": ["<p>", "tags", "tag", "paragraph"]},
+
+  { "user": "orangutan",
+    "question": "Aaaaaah this is so difficult! I don't know how to do anything!",
+    "answers": [
+      { "user": "longhorn", "text": "Where here to help you learn! You'll get there with practice, just like you learned how to walk, talk and write and draw and ...oh so many things!"}
+      ],
+    "tags": ["can't", "difficult", "panic"]},
+
+  { "user": "orangutan",
+    "question": "my html is not showing up",
+    "answers": [
+        { "user": "longhorn", "text": "Let's debug it! When something is not doing what you wanted it to, or something breaks, you should debug it. First, go through your code line by line and see if their are any spelling errors, migging closing tags, etc. Try that. If it still doesn't work, ask for help!"}
+        ],
+    "tags": ["debug"]},
+
+    { "user": "orangutan",
+    "question": "I'm using a header tag and my font is too big!",
+    "answers": [
+      { "user": "squirrel", "text": "Header tags are meant to make headlines stand out. They come in 5 sizes, <h1> through <h5>, try differnt h tags to see what fits. "}
+      ],
+    "tags": ["<h>", "header", "tags"]},
+
+
+  { "user": "orangutan",
+    "question": "changing text style",
+    "answers": [
+      { "user": "llama", "text": "Here are some properties to set text size and color: 'color' sets the color of text, 'text-transform' can make everything upper or lower case. I'll show you one example, you can try the other. p { text-transform: uppercase; " }
+      ],
+    "tags": ["css", "css basic properties", "styles"]},
+
+  { "user": "orangutan",
+    "question": "changing background color or image",
+    "answers": [
+        { "user": "llama", "text": " Inside the tag that you want the background color property on add this property - value pair: background-color: green; "}
+        ],
+    "tags": ["css"]},
+
+  { "user": "orangutan",
+    "question": "css properties",
+    "answers": [
+        { "user": "llama", "text": "Try some of these properties. I've listed property name and its value, examples are separated by commas. Remember these property-value pairs go inside curly braces defining style for a tag like div or paragraph. color: blue, background-color: orange; , border-right-style: solid; , border-right-color: black; , visibility: hidden; , "}
+        ],
+    "tags": ["css", "properties", "css basic properties"]},
+
+  { "user": "orangutan",
+    "question": "Give me some ideas! What do I do now that I have finished my project and I know all about paragraph, header,list and div tags?",
+    "answers": [
+      { "user": "llama", "text": "Try to change color of your text in different <div>, <h> or <p> tags! Search for 'css color' "},
+      { "user": "dumbo octopus", "text": "Try to do something funny. Like a disappearing text. I'll let you figure this one out..."}, 
+      { "user": "squirrel", "text": "How about making your text the same color as your background color? That'll be fun :) ."}, 
+      { "user": "snowy owl", "text": "Play around with more properties of css. To get more properties, search for 'css basic properties' and then put that in your code like you did with colors and see what happens.!"}
+ 
+      ],
+    "tags": ["ideas"]}
+
+  
+
+
